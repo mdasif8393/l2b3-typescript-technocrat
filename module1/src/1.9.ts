3570e84f43380c34c34d20e40e111f81fed39bb6
mdasif8393 <md.asif.8393@gmail.com>
2024-05-01 22:57:43 +0600

    type alish 1.9.ts

---
+++ module1/src/1.9.ts
@@ -0,0 +1,27 @@
+{
+  // Type Alias
+
+  type Student = {
+    name: string;
+    age: number;
+    gender: string;
+    contactNo?: string;
+    address: string;
+  };
+
+  const student1: Student = {
+    name: "Alias",
+    age: 12,
+    gender: "132",
+    contactNo: "2323",
+    address: "ascf asdf awdf",
+  };
+
+  type UserName = string;
+
+  const userName: UserName = "Persian";
+
+  // function alias
+  type Add = (num1: number, num2: number) => number;
+  const add: Add = (num1, num2) => num1 + num2;
+}
