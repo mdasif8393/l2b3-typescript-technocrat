139f0a7180e7ddc8c1e31c3c7f8e7054b1ada7fc
mdasif8393 <md.asif.8393@gmail.com>
2024-05-03 12:24:18 +0600

    2.2.ts: Interface, Type Vs Interfac2-3: Introduction To Generics

---
+++ module2/2.3.ts
@@ -0,0 +1,22 @@
+{
+  // 2-3: Introduction To Generics
+
+  type GenericArray<T> = Array<T>;
+
+  const rollNumbers: GenericArray<number> = [1, 2, 3, 4, 5];
+  const mentors: GenericArray<string> = ["a", "b", "c", "d", "e"];
+  const boolArray: GenericArray<boolean> = [true, false, false];
+}
+
+const user: GenericArray<{ name: string; age: number }> = [
+  {
+    name: "John",
+    age: 22,
+  },
+];
+
+// Generic Tuple
+
+type GenericTuple<X, Y> = [X, Y];
+
+const manush: GenericTuple<string, string> = ["a", "s"];
