1ab0228b0dcadb35fc4d6562702d800c3d3ac27f
mdasif8393 <md.asif.8393@gmail.com>
2024-05-03 11:18:56 +0600

    2.2.ts: Interface, Type Vs Interface

--- module2/2.1.ts
+++ module2/2.1.ts
@@ -1,35 +1,32 @@
-{
-  //
-  let anything: any;
-  anything = "Next Level Web Developer";
-  anything = 3333;
-  anything as number;
+// {
+//   //
+//   let anything: any;
+//   anything = "Next Level Web Developer";
+//   anything = 3333;
+//   anything as number;
 
+//   const kgToGm = (value: string | number): string | number | undefined => {
+//     if (typeof value === "string") {
+//       const convertedValue = parseFloat(value) * 1000;
+//       return `The converted value is ${convertedValue}`;
+//     }
+//     if (typeof value === "number") {
+//       return value * 1000;
+//     }
+//   };
 
+//   const result1 = kgToGm(1000) as number;
+//   const result2 = kgToGm("2000") as string;
 
-  const kgToGm = (value: string | number): string | number | undefined => {
-    if (typeof value === "string") {
-      const convertedValue = parseFloat(value) * 1000;
-      return `The converted value is ${convertedValue}`;
-    }
-    if (typeof value === "number") {
-      return value * 1000;
-    }
-  };
+//   type CustomError = {
+//     message: string
+//   }
 
-  const result1 = kgToGm(1000) as number;
-  const result2 = kgToGm("2000") as string;
+//   try{
 
+//   }
+//   catch((error as CustomError).message){
 
-  type CustomError = {
-    message: string
-  }
-
-  try{
-
-  }
-  catch((error as CustomError).message){
-
-  }
-  //
-}
+//   }
+//   //
+// }
