f4a1270c34937a5143da9f0f9e795b8e04e835d2
mdasif8393 <md.asif.8393@gmail.com>
2024-05-04 15:13:11 +0600

    2-7 Constraint Using Key Of

--- module2/2.7.ts
+++ module2/2.7.ts
@@ -1,4 +1,31 @@
 {
-  // Constraint Using Key Of
+  // generic Constraint Using Key Of
+
+  type Vehicle = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
+
+  //  bike | car | ship
+  type Owner = bike | car | ship;
+  type Owner1 = keyof Vehicle;
+
+  const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+    return obj[key];
+  };
+
+  const user = {
+    name: "Mr. Parsin",
+    age: 26,
+    address: "ctg",
+  };
+  const car = {
+    name: "Mr. Parsin",
+    age: 26,
+    address: "ctg",
+  };
+  const result1 = getPropertyValue(user, "name");
+
   //
 }
