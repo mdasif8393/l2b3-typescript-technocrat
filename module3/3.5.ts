045522f42c38c555e4d40b0e8a4004b87313ff7b
mdasif8393 <md.asif.8393@gmail.com>
2024-05-06 15:28:53 +0600

    3.5.ts Access Modifiers

---
+++ module3/3.5.ts
@@ -0,0 +1,33 @@
+{
+  // access modifier
+  class BankAccount {
+    readonly id: number;
+    name: string;
+    protected _balance: number;
+
+    constructor(id: number, name: string, _balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = _balance;
+    }
+
+    addDeposit(amount: number) {
+      this._balance += this._balance + amount;
+    }
+
+    getBalance() {
+      return this._balance;
+    }
+  }
+
+  class StudentAccount extends BankAccount {
+    text() {}
+  }
+
+  const goribManush = new BankAccount(11, "Asif", 1000);
+  goribManush.addDeposit(300);
+  const myBalance = goribManush.getBalance();
+  console.log(myBalance);
+
+  //
+}
